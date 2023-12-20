937ab36613ffbce0f79758939884a3bf840949b3
Jeremy Klein <jklein24@gmail.com>
2023-12-19 21:47:35 -0800

    Tweak conversion mechanics to be more explicit and safe.
    - Make the decimals field on `Currency` required and change its description to include more details about its use.
    - Change the multiplier field from i64 to f64 to allow for very small unit currencies. See https://github.com/uma-universal-money-address/protocol/blob/main/umad-04-lnurlp-response.md for details on why this is needed.
    
    NOTE: This is technically a breaking change. Given that we're not yet at UMA 1.0, I'm bumping the protocol version here to 0.2 to indicate the bump and to be able to tell what version the counterparty is using for debugging.

--- src/currency.rs
+++ src/currency.rs
@@ -2,7 +2,8 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct Currency {
-    // The ISO 4217 currency code of the currency (eg. USD).
+    // The ISO 4217 (if applicable) currency code (eg. "USD"). For cryptocurrencies, this will  be a ticker
+    // symbol, such as BTC for Bitcoin.
     pub code: String,
 
     // The full display name of the currency (eg. US Dollars).
@@ -13,7 +14,7 @@ pub struct Currency {
 
     // The estimated millisatoshis per smallest "unit" of this currency (eg. 1 cent in USD).
     #[serde(rename = "multiplier")]
-    pub millisatoshi_per_unit: i64,
+    pub millisatoshi_per_unit: f64,
 
     // The minimum amount of the currency that can be sent in a single transaction. This is in the
     // smallest unit of the currency (eg. cents for USD).
@@ -25,10 +26,12 @@ pub struct Currency {
     #[serde(rename = "maxSendable")]
     pub max_sendable: i64,
 
-    // The number of digits after the decimal point for display on the sender side. For example,
-    // in USD, by convention, there are 2 digits for cents - $5.95. in this case, `decimals`
-    // would be 2. Note that the multiplier is still always in the smallest unit (cents). This field
-    // is only for display purposes. The sender should assume zero if this field is omitted, unless
-    // they know the proper display format of the target currency.
-    pub decimals: Option<i64>,
+    // The number of digits after the decimal point for display on the sender side, and to add clarity
+    // around what the "smallest unit" of the currency is. For example, in USD, by convention, there are 2 digits for
+    // cents - $5.95. In this case, `decimals` would be 2. Note that the multiplier is still always in the smallest
+    // unit (cents). In addition to display purposes, this field can be used to resolve ambiguity in what the multiplier
+    // means. For example, if the currency is "BTC" and the multiplier is 1000, really we're exchanging in SATs, so
+    // `decimals` would be 8.
+    // For details on edge cases and examples, see https://github.com/uma-universal-money-address/protocol/blob/main/umad-04-lnurlp-response.md.
+    pub decimals: i64,
 }
